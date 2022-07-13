d613d9b9ce0658b89fd76e1d4f707684c06e6a1a
Iiro Alhonen <iiro.alhonen@sanoma.com>
2022-07-13 16:00:42 +0300

    Basis of Nifty Plugins.

---
+++ Plugins/SortImports/plugin.swift
@@ -0,0 +1,12 @@
+//
+//  Created by Iiro Alhonen on 13.7.2022.
+//
+
+import PackagePlugin
+
+@main
+struct SortImports: CommandPlugin {
+    func performCommand(context: PackagePlugin.PluginContext, arguments: [String]) async throws {
+        // ...
+    }
+}
